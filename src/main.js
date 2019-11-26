fddfedc7a9ab61c1adc757be458ae68b5b431f8f
Acmon Soares <acmon.soares@afil.io>
2019-11-26 20:17:38 -0300

    adiciona rota do content de knights

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
+
+import ContentListKnights from './components/ContentListKnights';
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
+    { path: '/get-knights', component: ContentListKnights },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes
 })
 
 new Vue({
