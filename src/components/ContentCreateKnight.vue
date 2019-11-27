6abdc5e78f7447c6e438e45d9f56a1ee14c8097c
Acmon Soares <acmon.soares@gmail.com>
2019-11-27 08:31:34 -0300

    formulario

--- src/components/ContentCreateKnight.vue
+++ src/components/ContentCreateKnight.vue
@@ -2,43 +2,56 @@
     <div>
         <v-subheader class="title">Cadastrar Knight</v-subheader>
         
-        <v-row class="mx-auto" align="center">
+        <v-card>
             
-            <v-form
-                ref="form"
-                v-model="valid"
-                :lazy-validation="lazy"
-            >
-                <v-text-field
-                    v-model="nameValue"
-                    label="Name"
-                    required
-                />
-    
-                <v-text-field
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-                ></v-text-field>
-    
-                <v-select
-                    v-model="select"
-                    :items="items"
-                    :rules="[v => !!v || 'Item is required']"
-                    label="Item"
-                    required
-                ></v-select>
-    
-                <v-checkbox
-                    v-model="checkbox"
-                    :rules="[v => !!v || 'You must agree to continue!']"
-                    label="Do you agree?"
-                    required
-                ></v-checkbox>
-    
-            </v-form>
-        </v-row>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Nome" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Apelido" hint="example of helper text only on focus"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                                label="Legal last name*"
+                                hint="example of persistent helper text"
+                                persistent-hint
+                                required
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Email*" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Password*" type="password" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-select
+                                :items="['0-17', '18-29', '30-54', '54+']"
+                                label="Age*"
+                                required
+                            ></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                            <v-autocomplete
+                                :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                                label="Interests"
+                                multiple
+                            ></v-autocomplete>
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <small>*indicates required field</small>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+            </v-card-actions>
+        </v-card>
     </div>
     
 </template>
