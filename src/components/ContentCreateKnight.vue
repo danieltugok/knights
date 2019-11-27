0088c03fc17443e3619e6d9e0df7951f85fb57bf
Acmon Soares <acmon.soares@gmail.com>
2019-11-27 03:19:45 -0300

    formulario

--- src/components/ContentCreateKnight.vue
+++ src/components/ContentCreateKnight.vue
@@ -1,13 +1,58 @@
 <template>
     <div>
         <v-subheader class="title">Cadastrar Knight</v-subheader>
-    </div>
+        
+        <v-row class="mx-auto" align="center">
+            
+            <v-form
+                ref="form"
+                v-model="valid"
+                :lazy-validation="lazy"
+            >
+                <v-text-field
+                    v-model="nameValue"
+                    label="Name"
+                    required
+                />
     
+                <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                ></v-text-field>
+    
+                <v-select
+                    v-model="select"
+                    :items="items"
+                    :rules="[v => !!v || 'Item is required']"
+                    label="Item"
+                    required
+                ></v-select>
+    
+                <v-checkbox
+                    v-model="checkbox"
+                    :rules="[v => !!v || 'You must agree to continue!']"
+                    label="Do you agree?"
+                    required
+                ></v-checkbox>
+    
+            </v-form>
+        </v-row>
+    </div>
     
 </template>
 
 <script>
+export default {
+    
+    data() {
+        return {
+            nameValue: null,
+        }
+    }
 
+}
 </script>
 <style>
    
