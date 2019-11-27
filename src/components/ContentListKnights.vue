a50f1ad09cab316a928b0679fa24b43868f72ab1
Acmon Soares <acmon.soares@afil.io>
2019-11-26 21:35:22 -0300

    formata idade e quantidade de armas

--- src/components/ContentListKnights.vue
+++ src/components/ContentListKnights.vue
@@ -7,7 +7,23 @@
                 :items="knightList"
                 :no-data-text="'Nenhum item encontrado'"
                 item-key="name"
-            />
+            >
+                <template v-slot:item.age="{ knight }">
+                    {{ formatAge(knight) }}
+                </template>
+
+                <template v-slot:item.weaponsAmount="{ knight }">
+                    {{ formatWeaponsAmount(knight.weapons) }}
+                </template>
+
+                <template v-slot:item.atack="{ knight }">
+                    {{ formatAttack(knight) }}
+                </template>
+
+                <template v-slot:item.exp="{ knight }">
+                    {{ formatExt(knight) }}
+                </template>
+            </v-data-table>
         </div>
     </div>
 </template>
@@ -21,10 +37,10 @@ export default {
             headers: [
                 { text: 'Nome'    , value: 'name', },
                 { text: 'Idade'   , value: 'age' },
-                { text: 'Armas'   , value: 'name' },
+                { text: 'Armas'   , value: 'weaponsAmount' },
                 { text: 'Atributo', value: 'keyAttribute' },
-                { text: 'Ataque'  , value: 'name' },
-                { text: 'Exp'     , value: 'name' },
+                { text: 'Ataque'  , value: 'atack' },
+                { text: 'Exp'     , value: 'exp' },
             ],
 
             knightList: []
@@ -47,6 +63,42 @@ export default {
                 .catch( (error) => {
                     console.error(error);
                 })
+        },
+
+        formatAge(knight) {
+            //NOTA - a título de avaliação, decidi não usar o moment.js para obter essa informação.
+            
+            const today = new Date(),
+                  splittedKnightBirthdayDate = knight.birthday.split("/"),
+                  knightBirthdayDateObject = new Date(+splittedKnightBirthdayDate[2], splittedKnightBirthdayDate[1] - 1, +splittedKnightBirthdayDate[0]),
+                  
+                  knightBirthdayDay = knightBirthdayDateObject.getDate(),
+                  knightBirthdayMonth = knightBirthdayDateObject.getMonth() + 1,
+                  knightBirthdayYear = knightBirthdayDateObject.getFullYear()
+
+            let age = today.getFullYear() - knightBirthdayYear;
+             
+            if (knightBirthdayMonth === today.getMonth()+1 ) {
+                if ( knightBirthdayDay > today.getDate() ) {
+                    age -= 1
+                }
+            } else if (knightBirthdayMonth > today.getMonth()+1) {
+                age -= 1
+            }
+            return age;
+
+        },
+
+        formatWeaponsAmount(weapons){
+            return weapons.length;
+        },
+
+        formatAttack(knight){
+            return 'ataque'
+        },
+
+        formatExp(knight){
+            return 'ataque'
         }
 
     },
